829525d4db8e3b50cdabdfe68c2e8768cbff0083
thewesternsite <115625531+thewesternsite@users.noreply.github.com>
2022-12-19 15:53:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,15 +2,31 @@ const btn = document.getElementById("btn-toast");
 const toast = document.getElementById("show-toast");
 const closeBtn = document.getElementById("close-btn");
 
+let clicked = false;
+
 const showToast = () => {
-    toast.style.display = "block";
-    btn.removeEventListener("click", showToast);
-    setTimeout(() => {
-        toast.style.display = "none";
-    }, 2000);
+    if(!clicked) {
+        clicked = true;
+        toast.style.display = "block";
+        setTimeout(() => {
+            toast.style.display = "none";
+            clicked = false;
+        }, 2000);
+    }
 }
 
 const closeToast = () => toast.style.display = "none";
 
 btn.addEventListener("click", showToast);
 closeBtn.addEventListener("click", closeToast)
+
+
+
+//addind and removing eventlistner
+// const showToast = () => {
+//     toast.style.display = "block";
+//     btn.removeEventListener("click", showToast);
+//     setTimeout(() => {
+//         toast.style.display = "none";
+//     }, 2000);
+// }
