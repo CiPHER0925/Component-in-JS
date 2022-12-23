683240f231f14a76101cd9ce0e4827a365638e8e
thewesternsite <115625531+thewesternsite@users.noreply.github.com>
2022-12-24 02:52:20 +0530

    Update : added tooltip component

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-const btn = document.getElementById("btn-toast");
-const toast = document.getElementById("show-toast");
-const closeBtn = document.getElementById("close-btn");
+const toastComponent = () => {
+    const btn = document.getElementById("btn-toast");
+    const toast = document.getElementById("show-toast");
+    const closeBtn = document.getElementById("close-btn");
 
-let clicked = false;
+    let clicked = false;
 
-const showToast = () => {
-    if(!clicked) {
-        clicked = true;
-        toast.style.display = "block";
-        setTimeout(() => {
-            toast.style.display = "none";
-            clicked = false;
-        }, 2000);
+    const showToast = () => {
+        if(!clicked) {
+            clicked = true;
+            toast.style.display = "block";
+            setTimeout(() => {
+                toast.style.display = "none";
+                clicked = false;
+            }, 2000);
+        }
     }
+
+    const closeToast = () => toast.style.display = "none";
+
+    btn.addEventListener("click", showToast);
+    closeBtn.addEventListener("click", closeToast);
 }
-
-const closeToast = () => toast.style.display = "none";
-
-btn.addEventListener("click", showToast);
-closeBtn.addEventListener("click", closeToast)
+toastComponent();
 
 
 //addind and removing eventlistner
@@ -29,3 +32,16 @@ closeBtn.addEventListener("click", closeToast)
 //         toast.style.display = "none";
 //     }, 2000);
 // }
+
+
+//tooltip
+const closeTooltip = () => {
+    const btn = document.getElementById("close-btn-tooltip");
+    const tooltip = document.getElementById("close-text");
+
+    const closeText = () => {
+        tooltip.style.display = "none";
+    }
+    btn.addEventListener("click", closeText);
+}
+closeTooltip();
